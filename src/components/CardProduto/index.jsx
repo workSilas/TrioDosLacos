e07439048b71a8909521024e3c7c35b8d2d755fb
Silas <ssilas102@gmail.com>
2024-10-30 14:39:15 -0300

    -Catalálogo Finalizado.
    -NPM atualizado.

--- src/components/CardProduto/index.jsx
+++ src/components/CardProduto/index.jsx
@@ -1,43 +1,36 @@
 import './index.scss';
-import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function CardProduto(props) {
-
-  const [produtos, setProdutos] = useState([])
+  const [produtos, setProdutos] = useState([]);
 
   async function buscarProdutos() {
-    let url = `http://localhost:3030/tdl/produtos/consulta/${props.sessao}`
-    let produtosEncontrados = await axios.post(url)
-    setProdutos(produtosEncontrados.data)
+    let url = `http://localhost:3030/tdl/produtos/consulta/${props.sessao}`;
+    let produtosEncontrados = await axios.post(url);
+    setProdutos(produtosEncontrados.data);
   }
 
   useEffect(() => {
-    buscarProdutos()
-  },[])
-
-  const navigate = useNavigate()
+    buscarProdutos();
+  }, [props.sessao]);
 
   return (
     <div className="CardProduto">
-      {produtos.map(item =>
-        <div className="card">
-          <div id='imagem' className="separacaoInfo" >
+      {produtos.map(item => (
+        <div className="card" key={item.id}>
+          <div id='imagem' className="separacaoInfo">
             <img src={item.imagem} alt="produto" />
           </div>
-
           <div className="separacaoInfo">
             <p>#{item.id}</p>
             <h5>{item.nome.length > 25 ? item.nome.substr(0, 9) + "." : item.nome}</h5>
             <h5>R${item.valor.toFixed(2)}</h5>
-            <button onClick={navigate("/Produto", {
-              id: item.id
-            })}>VER</button>
+            <Link to="/PaginaProduto" state={{ id: item.id, name: item.nome }}>VER</Link>
           </div>
         </div>
-      )}
-
+      ))}
     </div>
   );
 }
--- src/pages/user/PaginaProduto/index.jsx
+++ src/pages/user/PaginaProduto/index.jsx
@@ -1,49 +1,51 @@
 import './index.scss';
 import Nav from '../../../components/Nav';
 import Rodape from '../../../components/Rodape';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function PaginaProduto() {
 
-  const [produtoEncontrado, setProdutoEncontrado] = useState([])
+  const location = useLocation();
+  const data = location.state;
+
+  const [produtoEncontrado, setProdutoEncontrado] = useState([]);
 
   async function buscar() {
-    let url = `http://localhost:3030/tdl/produtos/consultaId/22`
-    let produtos = await axios.post(url)
-    console.log(produtos);
-    
-    setProdutoEncontrado(produtos.data)
+    let url = `http://localhost:3030/tdl/produtos/consultaId/${data.id}`; 
+    let produtos = await axios.post(url);
+    setProdutoEncontrado(produtos.data);
   }
 
   useEffect(() => {
-   buscar()
-  }, [])
+    if (data && data.id) {
+      buscar();
+    }
+  }, [data]);
 
   return (
     <div className="PaginaProduto">
-      <Nav
-        titulo="Produto"
-      />
+      <Nav titulo="Produto" />
 
-      {produtoEncontrado.map(item =>
-        <div className="informacoesDoproduto">
+      {produtoEncontrado.map(item => (
+        <div className="informacoesDoproduto" key={item.id}> 
           <Link to={'/'}>VOLTAR</Link>
           <div className="alinharInfo">
             <img src={item.imagem} alt="produto" />
             <div className="infoDeCompra">
               <h1>{item.nome}</h1>
               <h2>R${item.valor.toFixed(2)}</h2>
-              <div><Link to={`https://wa.me/5511977798407?text=Quero saber mais sobre o produto: ID ${item.id} Nome ${item.nome} Valor: R$${item.valor.toFixed(2)}`}>SOLICITAR</Link></div>
+              <div>
+                <Link to={`https://wa.me/5511977798407?text=Quero saber mais sobre o produto: ID ${item.id} Nome ${item.nome} Valor: R$${item.valor.toFixed(2)}`}>SOLICITAR</Link>
+              </div>
             </div>
           </div>
           <p>Quantidade disponível({item.quantidade})</p>
           <h1>Descrição</h1>
           <p>{item.descricao}</p>
         </div>
-      )}
-  
+      ))}
       <Rodape />
     </div>
   );
