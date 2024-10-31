580cb77e89c8723b9171c8fdcc93f42eafd628c9
Lucas Campos <165829685+oLuccas@users.noreply.github.com>
2024-10-31 11:08:34 +0000

    Adicionada Carrossel | Mudança nos botões do HOME | Alinhamento no centro das imagens do instagram e facebook do Rodapé

--- src/pages/user/Home/index.jsx
+++ src/pages/user/Home/index.jsx
@@ -3,6 +3,7 @@ import Nav from '../../../components/Nav';
 import Rodape from '../../../components/Rodape';
 import { Link } from 'react-router-dom';
 import BotaoAdm from '../../../components/BotaoAdm';
+import Carrossel from '../../../components/Carrossel';
 
 export default function Home() {
   return (
@@ -11,10 +12,8 @@ export default function Home() {
         titulo="Bem-Vindo(a)!"
       />
       <BotaoAdm />
-
-      <div className="sessaoInicialCarrossel">
-
-      </div>
+      
+      <Carrossel />
 
       <div className="sessaoDescubra">
         <h1>CONHEÇA NOSSAS COLEÇÕES</h1>
@@ -24,63 +23,80 @@ export default function Home() {
             <img src="/assets/images/LaçoDecorado.png" alt="Laço Decorado" />
             <p>Laços Decorados</p>
           </div>
+
           <div className="alinharPropiedades">
             <p> Coleção de laços <br />
               decorados com enfeites diversos </p>
-            <Link to={'/CatalogoLacosDecorados'}>VER</Link>
+            <Link to={'/CatalogoLacosDecorados'} className='botao'>VER</Link>
           </div>
+
         </div>
+
         <div id='trocaDePopriedades' className="colecoesDisplay">
+
           <div className="alinharPropiedades">
             <img src="/assets/images/FaixaBebe.png" alt="Faixas de Bêbe" />
             <p>Faixas de Bebê</p>
           </div>
+
           <div className="alinharPropiedades">
             <p> Faixas estilizadas e <br />
               adoráveis  </p>
-            <Link to={'/CatalogoFaixasDeBebe'}>VER</Link>
+            <Link to={'/CatalogoFaixasDeBebe'} className='botao'>VER</Link>
           </div>
+
         </div>
+
         <div className="colecoesDisplay">
+
           <div className="alinharPropiedades">
             <img src="/assets/images/LaçoEstampado.png" alt="Laço Estampado" />
             <p>Laços Estampados</p>
           </div>
+
           <div className="alinharPropiedades">
             <p> Laços  com inumeas estampas, <br />
               coloridas e leves </p>
-            <Link to={'/CatalogoLacosEstampados'}>VER</Link>
+            <Link to={'/CatalogoLacosEstampados'} className='botao'>VER</Link>
           </div>
         </div>
+
         <div id='trocaDePopriedades' className="colecoesDisplay">
+
           <div className="alinharPropiedades">
             <img src="/assets/images/Kits.png" alt="Kits de Laços" />
             <p>Kits de Laços</p>
           </div>
+
           <div className="alinharPropiedades">
             <p> Kits com os mais variados <br />
               conjuntos de laços s </p>
-            <Link to={'/CatalogoKitsDeLacos'}>VER</Link>
+            <Link to={'/CatalogoKitsDeLacos'} className='botao'>VER</Link>
           </div>
+
         </div>
+
       </div>
 
       <div className="sessaoEncomendas">
         <p>Não encontrou nada que te agrade? <br />
           Faça sua <b style={{ color: "#FFB099" }}>própria configuração! Use da <br />
             sua imaginação para vestir seu bebê.</b></p>
-        <Link to={'/Encomendas'}>ENCOMENDAR</Link>
+        <Link to={'/Encomendas'} className='botao'>ENCOMENDAR</Link>
       </div>
 
       <div className="sessoDeVoltaAoCatalogo">
+
         <div className="alinharComponentes">
           <p>Ainda não viu nossos produtos? <br />
             Explore nosso catálogo recheado <br />
             de acessórios que darão brilho <br />
             ao seu bebê!</p>
-          <Link to={'/CatalogoLacosDecorados'}>CATÁLOGO</Link>
+          <Link to={'/CatalogoLacosDecorados'} className='botao'>CATÁLOGO</Link>
         </div>
+
         <img src="/assets/images/laçoVetorzado.png" alt="Laço" />
+
       </div>
 
       <Rodape />
