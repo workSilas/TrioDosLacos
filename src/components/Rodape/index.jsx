89ecd0060b6caea46b595e2bc254d7c55c16ad60
workSilas <worksilas102@gmail.com>
2024-11-05 10:37:12 -0300

    -Rodapé alinhado

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -10,21 +10,24 @@ export default function Rodape() {
         <Link to={"/Entrar"}><img src="/assets/images/LogoOficial.png" alt="logo" /></Link>
       </div>
 
-      <div className="separacaoComponentesRodape">
-        <h1>SUPORTE</h1>
-        <Link target='_blank' to={"https://wa.me/5511977798407?text=Olá! Gostaria de saber mais sobre seus serviços."}>Contato</Link>
-      </div>
+      <div className="alinhamentoTopo">
 
-      <div className="separacaoComponentesRodape">
-        <h1>POLÍTICAS</h1>
-        <Link to={"/"}>Termos de Uso</Link>
-      </div>
+        <div className="separacaoComponentesRodape">
+          <h1>SUPORTE</h1>
+          <Link target='_blank' to={"https://wa.me/5511977798407?text=Olá! Gostaria de saber mais sobre seus serviços."}>Contato</Link>
+        </div>
 
-      <div className="separacaoComponentesRodape">
-        <h1>SIGA-NOS</h1>
-        <div className="alinhamentoImagens">
-          <Link target='_blank' to={"https://www.instagram.com/trio_dos_lacos/"}><img src="/assets/images/Instagram.png" alt="instagam" /></Link>
-          <Link target='_blank' to={"https://www.facebook.com/profile.php?id=100087909888766"}><img src="/assets/images/Facebook.png" alt="facebook" /></Link>
+        <div className="separacaoComponentesRodape">
+          <h1>POLÍTICAS</h1>
+          <Link to={"/"}>Termos de Uso</Link>
+        </div>
+
+        <div className="separacaoComponentesRodape">
+          <h1>SIGA-NOS</h1>
+          <div className="alinhamentoImagens">
+            <Link target='_blank' to={"https://www.instagram.com/trio_dos_lacos/"}><img src="/assets/images/Instagram.png" alt="instagam" /></Link>
+            <Link target='_blank' to={"https://www.facebook.com/profile.php?id=100087909888766"}><img src="/assets/images/Facebook.png" alt="facebook" /></Link>
+          </div>
         </div>
       </div>
     </div>
