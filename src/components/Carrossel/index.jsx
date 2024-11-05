82adc294727a979ab1e7dec32953dafa20155c52
workSilas <worksilas102@gmail.com>
2024-11-05 10:22:16 -0300

    Carrosel em alteração

--- src/components/Carrossel/index.jsx
+++ src/components/Carrossel/index.jsx
@@ -4,8 +4,8 @@ import './index.scss';
 export default function Carrossel() {
     const [currentIndex, setCurrentIndex] = useState(0);
     const images = [
+        '/assets/images/bebeQuatroK.png',
         '/assets/images/bebeCarrosselFaixaRosa.png',
-        '/assets/images/bebeCarrosselFaixaRosa2.png',
     ];
 
     useEffect(() => {
