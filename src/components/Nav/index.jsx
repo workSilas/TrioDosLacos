a0b146a9bbc57647ed347abd42aa14d079f9e9f3
bruhsvicente <brutronica931@gmail.com>
2024-11-19 20:48:51 +0000

    Nav alterada

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,6 +1,9 @@
 import './index.scss';
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Popup from '../Popup';
+import { urlApi } from '../../config/urlApi';
 
 export default function Nav(props) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -8,6 +11,47 @@ export default function Nav(props) {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const [mostrarPopup, setMostrarPopup] = useState(false);
+  const [mensagem, setMensagem] = useState("")
+
+  const popup = () => {
+    setMostrarPopup(!mostrarPopup)
+  }
+
+  const [cadastrado, setCadastrado] = useState(false)
+
+  useEffect(() => {
+    let token = localStorage.getItem('USUARIO')
+
+    if (token == null || token == undefined) {
+      setCadastrado(false)
+    } else {
+      autenticarUsuário(token)
+    }
+  }, [])
+
+  async function autenticarUsuário(token) {
+    let url = `${urlApi}/tdl/usuarios/autenticar?x-access-token=${token}`
+
+    try {
+      let resp = await axios.get(url)
+
+      if (resp.data.erro !== undefined || resp.data.erro !== null) {
+        setCadastrado(true)
+      } else {
+        setMensagem('Erro ao autenticar Usuário: ', resp.data.erro)
+        popup()
+        setCadastrado(false)
+      }
+    }
+    catch (error) {
+      setMensagem('Erro ao autenticar Usuário: ', error)
+      popup()
+      setCadastrado(false)
+    }
+  }
+
 
   return (
     <header className='Nav'>
@@ -19,11 +63,35 @@ export default function Nav(props) {
         </div>
 
         <nav className={`menu ${window.innerWidth > 768 ? 'visible' : 'hidden'}`}>
-          <div className='links'>
-            <Link to="/">Início</Link>
-            <Link to="/Encomendas">Encomendas</Link>
-            <Link to="/CatalogoLacosDecorados">Catálogo</Link>
-          </div>
+        {cadastrado ?
+            <div className='links'>
+              <Link to="/">Início</Link>
+              <Link to="/Encomendas">Encomendas</Link>
+              <Link to="/CatalogoLacosDecorados">Catálogo</Link>
+              <Link to="/Favoritos">Favoritos</Link>
+
+              <Link to="/MeuPerfil">
+                <div className='botao-perfil'>
+                  <img src="/assets/images/freepik-export-20241119171750qjuk.png" alt="Perfil do Usuário" />
+                  <p>Meu Perfil</p>
+                </div>
+              </Link>
+            </div>
+            :
+            <div className='links'>
+              <Link to="/">Início</Link>
+              <Link to="/Encomendas">Encomendas</Link>
+              <Link to="/CatalogoLacosDecorados">Catálogo</Link>
+              <Link to="/Login">Login</Link>
+
+              <Link to="/Cadastro">
+                <div className='botao-perfil'>
+                  <img src="/assets/images/freepik-export-20241119171750qjuk.png" alt="Perfil do Usuário" />
+                  <p>ENTRAR</p>
+                </div>
+              </Link>
+            </div>
+          }
         </nav>
 
         <div className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
@@ -35,14 +103,41 @@ export default function Nav(props) {
 
       {isMenuOpen && window.innerWidth <= 768 && (
         <nav className="menu-mobile">
-          <div className='links-mobile'>
-            <Link to="/">Início</Link>
-            <Link to="/Encomendas">Encomendas</Link>
-            <Link to="/CatalogoLacosDecorados">Catálogo</Link>
-          </div>
+          {cadastrado ?
+            <div className='links-mobile'>
+              <Link to="/">Início</Link>
+              <Link to="/Encomendas">Encomendas</Link>
+              <Link to="/CatalogoLacosDecorados">Catálogo</Link>
+              <Link to="/Favoritos">Favoritos</Link>
 
+              <Link to="/MeuPerfil">
+                <div className='botao-perfil'>
+                  <img src="/assets/images/freepik-export-20241119171750qjuk.png" alt="Perfil do Usuário" />
+                  <p>Meu Perfil</p>
+                </div>
+              </Link>
+            </div>
+            :
+            <div className='links-mobile'>
+              <Link to="/">Início</Link>
+              <Link to="/Encomendas">Encomendas</Link>
+              <Link to="/CatalogoLacosDecorados">Catálogo</Link>
+              <Link to="/Login">Login</Link>
+
+              <Link to="/Cadastro">
+                <div className='botao-perfil'>
+                  <img src="/assets/images/freepik-export-20241119171750qjuk.png" alt="Perfil do Usuário" />
+                  <p>ENTRAR</p>
+                </div>
+              </Link>
+            </div>
+          }
         </nav>
       )}
-    </header>
+
+      {mostrarPopup && (
+        <Popup mensagem={mensagem} funcao={popup} />
+      )}
+    </header >
   );
 }
